7843bcd3bde04dfa0d1d16ad2702b47734b3811a
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-11-11 20:36:09 +0000

    Green

--- spec/euler_fib_spec.rb
+++ spec/euler_fib_spec.rb
@@ -5,6 +5,10 @@ class FibSequenceGenerator
   
 end
 
+class FibSequenceGenerator
+  
+end
+
 class EvenFibSummator
   def sum
     FibSequenceGenerator.new
@@ -12,9 +16,16 @@ class EvenFibSummator
 end
 
 describe EvenFibSummator do
+  let(:fib_sequence_generator) { mock(FibSequenceGenerator) }
   let(:even_fib_summator) { EvenFibSummator.new }
-  it "creates FibSequenceGenerator" do
+
+  it "creates a FibSequenceGenerator" do
     FibSequenceGenerator.should_receive(:new)
+    even_fib_summator.sum
+  end
+  
+  it "creates a EvenPicker with the FibSequenceGenerator" do
+    EvenPicker.should_receive(:new).with(fib_sequence_generator)
     even_fib_summator.sum
   end
 end
