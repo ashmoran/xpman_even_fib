b968e48287f5164320aa23fee88ee2ea9b586b07
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-11-11 19:43:51 +0000

    Green

--- spec/euler_fib_spec.rb
+++ spec/euler_fib_spec.rb
@@ -3,16 +3,6 @@ require 'euler_fib'
 
 # 1 1 2 3 5 8 13 21 34 55 89 144 233 377
 
-def even_fib_sum(n)
-  if n < 3
-    0
-  else
-    2
-  end
-end
-
-describe "euler_fib" do
-  specify { even_fib_sum(1).should eq 0 }
-  specify { even_fib_sum(2).should eq 0 }
-  specify { even_fib_sum(3).should eq 2 }
+describe "even fib sum" do
+  
 end
