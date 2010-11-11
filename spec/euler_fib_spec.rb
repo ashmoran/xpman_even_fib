ae28d8ac71b20f52ef379b619753b15df41280b3
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-11-11 20:29:33 +0000

    Red

--- spec/euler_fib_spec.rb
+++ spec/euler_fib_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 require 'euler_fib'
 
-describe "euler_fib" do
-  it "does something" do
-    fail "needs some specs"
+describe EvenFibSummator do
+  let(:even_fib_summator) { EvenFibSummator.new }
+  it "creates FibSequenceGenerator" do
+    FibSequenceGenerator.should_receive(:new)
+    even_fib_summator.sum
   end
 end
