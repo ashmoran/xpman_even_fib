1dd6224eaac999b9361a0d4977f9bced20156439
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-11-11 19:49:53 +0000

    Green

--- spec/euler_fib_spec.rb
+++ spec/euler_fib_spec.rb
@@ -4,5 +4,12 @@ require 'euler_fib'
 # 1 1 2 3 5 8 13 21 34 55 89 144 233 377
 
 describe "even fib sum" do
+  let(:sequence_generator) { mock("sequence_generator", sequence: [1,2,3]) }
   
+  let(:summer) { Summer.new(sequence_generator) }
+  
+  it "adds numbers" do
+    summer.sum.should eq 6
+  end
 end
+
