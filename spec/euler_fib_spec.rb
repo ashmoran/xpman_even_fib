91fa811cb18cb4fdca3304ced6eb8e494ae3a82f
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-11-11 20:29:55 +0000

    Green

--- spec/euler_fib_spec.rb
+++ spec/euler_fib_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 require 'euler_fib'
 
+class FibSequenceGenerator
+  
+end
+
+class EvenFibSummator
+  
+end
+
 describe EvenFibSummator do
   let(:even_fib_summator) { EvenFibSummator.new }
   it "creates FibSequenceGenerator" do
