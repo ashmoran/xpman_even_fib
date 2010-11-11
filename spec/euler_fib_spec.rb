4c8996bd8dece007f76c2267e8f480e4d01d3763
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-11-11 22:25:40 +0000

    Green

--- spec/euler_fib_spec.rb
+++ spec/euler_fib_spec.rb
@@ -33,6 +33,15 @@ class EvenFibSummator
   end
 end
 
+describe EvenPicker do
+  let(:sequence_generator) { mock("SequenceGenerator", sequence: [1,2,3,4,5,6,7,8])}
+  let(:even_picker) { EvenPicker.new(sequence_generator) }
+  
+  it "picks even numbers" do
+    even_picker.sequence.should eq [2,4,6,8]
+  end
+end
+
 describe Summator do
   let(:sequence_generator) { mock("SequenceGenerator", sequence: [1,2,3])}
   let(:summator) { Summator.new(sequence_generator) }
