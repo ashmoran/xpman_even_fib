cdf0e42c711ccc81a2059648fb3039ab41547096
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-11-11 19:21:41 +0000

    Green

--- spec/euler_fib_spec.rb
+++ spec/euler_fib_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 require 'euler_fib'
 
+# 1 1 2 3 5 8 13 21 34 55 89 144 233 377
+
 describe "euler_fib" do
   it "does something" do
-    fail "needs some specs"
+    even_fib_sum(1).should eq 0
   end
 end
