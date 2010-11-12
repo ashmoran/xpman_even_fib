a4f0e5d7df15e2afc2d07b1f5a49352a05a273db
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-11-12 10:22:57 +0000

    Green

--- spec/euler_fib_spec.rb
+++ spec/euler_fib_spec.rb
@@ -31,6 +31,8 @@ describe LimitedSequenceGenerator do
   
   context "with a limit that is present in the sequence" do
     subject { LimitedSequenceGenerator.new(incremental_sequence_generator, 8) }
+    
+    it_satisfies_contract "SequenceGenerator"
 
     it "collects values until one value is returned that is >= than limit" do
       subject.sequence.should eq [1, 4, 3]
