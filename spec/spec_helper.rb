217d124b382a689e312b6238cd14e03559bade27
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-11-12 10:22:01 +0000

    Green

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,15 @@ require 'bundler'
 Bundler.require(:default, :test)
 
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../lib"))
+
+module SpecHelperObjectExtensions
+  def contract(name, &block)
+    shared_examples_for(name, &block)
+  end
+end
+
+include SpecHelperObjectExtensions
+
+RSpec.configure do |c|
+  c.alias_it_should_behave_like_to(:it_satisfies_contract, 'satisfies contract:')
+end
