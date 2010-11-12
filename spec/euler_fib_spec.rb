c641a8e8ff0c33abe395c7f246d171c5bc7870bf
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-11-12 10:15:52 +0000

    Green

--- spec/euler_fib_spec.rb
+++ spec/euler_fib_spec.rb
@@ -38,7 +38,7 @@ describe LimitedSequenceGenerator do
 end
 
 describe EvenPicker do
-  let(:subject) {
+  subject {
     EvenPicker.new(
       mock("SequenceGenerator", sequence: [1, 2, 3, 4, 5, 6, 7, 8])
     )
