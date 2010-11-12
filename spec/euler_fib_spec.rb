b18ec743627783d0a52bde1aa53d1d7ba6f2fe83
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-11-12 10:27:24 +0000

    Green

--- spec/euler_fib_spec.rb
+++ spec/euler_fib_spec.rb
@@ -1,6 +1,56 @@
 require 'spec_helper'
 require 'euler_fib'
 
+describe EvenFibSummator do
+  it "works" do
+    EvenFibSummator.new(35).sum.should eq 44
+  end
+  
+  it "works for 4 000 000" do
+    # Although I did actually use the EvenFibSummator to generate the answer :)
+    EvenFibSummator.new(4_000_000).sum.should eq 4613732
+  end
+end
+
+describe EvenFibSummator do
+  let(:summator) { mock(Summator, sum: 1) }
+  let(:even_picker) { mock(EvenPicker) }
+  let(:limited_sequence_generator) { mock(LimitedSequenceGenerator) }
+  let(:fib_sequence_generator) { mock(IncrementalFibSequenceGenerator) }
+  let(:even_fib_summator) { EvenFibSummator.new(2) }
+  
+  before(:each) {
+    IncrementalFibSequenceGenerator.stub(new: fib_sequence_generator)
+    LimitedSequenceGenerator.stub(new: limited_sequence_generator)
+    EvenPicker.stub(new: even_picker)
+    Summator.stub(new: summator)
+  }
+
+  it "creates a IncrementalFibSequenceGenerator" do
+    IncrementalFibSequenceGenerator.should_receive(:new)
+    even_fib_summator.sum
+  end
+  
+  it "creates a LimitedSequenceGenerator with the IncrementalFibSequenceGenerator and the limit" do
+    LimitedSequenceGenerator.should_receive(:new).with(fib_sequence_generator, 2)
+    even_fib_summator.sum
+  end
+  
+  it "creates a EvenPicker with the IncrementalFibSequenceGenerator" do
+    EvenPicker.should_receive(:new).with(limited_sequence_generator)
+    even_fib_summator.sum
+  end
+  
+  it "creates a Summator with the EvenPicker" do
+    Summator.should_receive(:new).with(even_picker)
+    even_fib_summator.sum
+  end
+  
+  it "asks the Summator for the sum" do
+    summator.should_receive(:sum)
+    even_fib_summator.sum.should eq 1
+  end  
+end
 describe IncrementalFibSequenceGenerator do
   let(:incremental_sequence_generator) { IncrementalFibSequenceGenerator.new }
   
@@ -77,54 +127,4 @@ end
 describe Summator do
   subject { Summator.new(mock("SequenceGenerator", sequence: [1, 2, 3])) }  
   its(:sum) { should eq 6 }
-end
-
-describe EvenFibSummator do
-  it "works" do
-    EvenFibSummator.new(35).sum.should eq 44
-  end
-  
-  it "works for 4 000 000" do
-    EvenFibSummator.new(4_000_000).sum.should eq 4613732
-  end
-end
-
-describe EvenFibSummator do
-  let(:summator) { mock(Summator, sum: 1) }
-  let(:even_picker) { mock(EvenPicker) }
-  let(:limited_sequence_generator) { mock(LimitedSequenceGenerator) }
-  let(:fib_sequence_generator) { mock(IncrementalFibSequenceGenerator) }
-  let(:even_fib_summator) { EvenFibSummator.new(2) }
-  
-  before(:each) {
-    IncrementalFibSequenceGenerator.stub(new: fib_sequence_generator)
-    LimitedSequenceGenerator.stub(new: limited_sequence_generator)
-    EvenPicker.stub(new: even_picker)
-    Summator.stub(new: summator)
-  }
-
-  it "creates a IncrementalFibSequenceGenerator" do
-    IncrementalFibSequenceGenerator.should_receive(:new)
-    even_fib_summator.sum
-  end
-  
-  it "creates a LimitedSequenceGenerator with the IncrementalFibSequenceGenerator and the limit" do
-    LimitedSequenceGenerator.should_receive(:new).with(fib_sequence_generator, 2)
-    even_fib_summator.sum
-  end
-  
-  it "creates a EvenPicker with the IncrementalFibSequenceGenerator" do
-    EvenPicker.should_receive(:new).with(limited_sequence_generator)
-    even_fib_summator.sum
-  end
-  
-  it "creates a Summator with the EvenPicker" do
-    Summator.should_receive(:new).with(even_picker)
-    even_fib_summator.sum
-  end
-  
-  it "asks the Summator for the sum" do
-    summator.should_receive(:sum)
-    even_fib_summator.sum.should eq 1
-  end  
 end
