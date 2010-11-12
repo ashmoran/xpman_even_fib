c61d121e6f826ab919a1ecaff65e45434d61cfa7
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-11-12 10:10:28 +0000

    Green

--- spec/euler_fib_spec.rb
+++ spec/euler_fib_spec.rb
@@ -20,9 +20,9 @@ end
 
 describe LimitedSequenceGenerator do
   let(:incremental_sequence_generator) {
-    isq = mock("IncrementalSequenceGenerator").stub(:next).and_return(1, 4, 3, 8, 12)
-    # isq
-    # isq
+    tap mock("IncrementalSequenceGenerator") do |isq|
+      isq.stub(:next).and_return(1, 4, 3, 8, 12)
+    end
   }
   
   context "with a limit that is present in the sequence" do
